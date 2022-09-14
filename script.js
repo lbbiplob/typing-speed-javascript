a59b048222b60497c0b8ef71778350ce21246cef
lbbiplob <biplab.jsnu@gmail.com>
2022-09-14 22:21:08 +0600

    button fiexed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -107,10 +107,11 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
-
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
+    
     // finished timer
     if (count == 0) {
+      countdownOverlay.style.display= 'none';
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
@@ -121,6 +122,7 @@ const start = () => {
     }
     count--;
   }, 1000);
+  
 };
 
 // START Countdown
